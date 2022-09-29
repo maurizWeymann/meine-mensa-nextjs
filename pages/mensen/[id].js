c8802a01be4d4e91ee89daded8a6837fd769f529
TwoFingers Catering <s0528279@htw-berlin.de>
2022-09-29 23:52:12 +0200

    solo mensa mit card

--- pages/mensen/[id].js
+++ pages/mensen/[id].js
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 export const getStaticPaths = async () => {
   const res = await fetch('https://openmensa.org/api/v2/canteens');
@@ -30,9 +37,27 @@ export const getStaticProps = async (context) => {
 const Details = ({ mensa }) => {
   return (
     <div>
-      <h1>{ mensa.name }</h1>
-      <p>{ mensa.city }</p>
-      <p>{ mensa.address }</p>
+      <Card sx={{ maxWidth: 1024 }}>
+                            <CardMedia
+                                component="img"
+                                height="180"
+                                image={`/mensafotos/${mensa.id}.jpg`}
+                                alt={mensa.name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                {mensa.name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                {mensa.address}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+  
     </div>
   )
 }
